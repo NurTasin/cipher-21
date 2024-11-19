bfc4715e589c6acaa53bc2cddb2494999bab8c81
NurTasin <nmuatasin2005@gmail.com>
2024-11-20 00:01:35 +0600

    Prisma Shits (4)

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,9 +1,6 @@
 "use client";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 export default function Home() {
-  return (
-    <div className="mx-auto">
-      Hello
-    </div>
-  );
+  return redirect("/dashboard");
 }
