26d4802f1b445435d5d40104d202bdf9119b1370
NurTasin <nmuatasin2005@gmail.com>
2024-11-19 22:23:10 +0600

    Prisma Shits

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,26 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getToken } from "firebase/messaging";
-import { messaging } from "@/lib/firebase";
-
 export default function Home() {
-  useEffect(() => {
-    ;(async ()=>{
-      if(localStorage.msgToken){
-        console.log("Token already exists");
-        return;
-      }
-      const notificationAccess = await Notification.requestPermission();
-      if(notificationAccess === "granted"){
-        console.log("Notification permission granted");
-        const token = await getToken(messaging, { vapidKey: process.env.VAPID });
-        console.log(token);
-        localStorage.msgToken = token;
-      }else{
-        alert("Notification permission not granted. You might not get important notifications.");
-      }
-    })()
-  },[]);
   return (
     <div className="mx-auto">
       Hello
--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ignored: ["**/node_modules", "**/devscripts", "**/Application Data/**"],
-    };
-
-    // Optional: If isServer (builds for server-side) fails
-    if (isServer) {
-      config.externals = ["react", ...config.externals];
-    }
-
-    return config;
-  },
   reactStrictMode: false,
+  experimental: {
+    outputFileTracingIgnores: ["./prisma/client/**/*"],
+  },
 };
 
 export default nextConfig;
